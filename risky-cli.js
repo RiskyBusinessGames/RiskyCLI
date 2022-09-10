67af3cc75f29ca2f572fe4e52a3d7cb01da985be
Matt Taylor <matt.grs@googlemail.com>
2022-09-10 13:37:08 +0100

    feat(project-tool): IGB SDK setup

--- risky-cli.js
+++ risky-cli.js
@@ -4,10 +4,107 @@ import {UnityProject} from "./src/UnityProject.js";
 
 const project = new UnityProject();
 
-const moduleName = "HelloWorldModule";
-const serviceName= "GreetingService";
-const componentName= "Potato";
+project.CreateModule("Plugins");
 
-project.CreateModule(moduleName);
-project.CreateComponent(moduleName, componentName);
-project.CreateService(moduleName, serviceName);
+project.CreateModule("Core");
+project.CreateService("Core", "ServiceProvider");
+
+project.CreateModule("Tracking");
+project.CreateService("Tracking", "TrackingService");
+project.CreateService("Tracking", "OptitrackTrackingService");
+project.CreateService("Tracking", "ViconTrackingService");
+project.CreateComponent("Tracking", "PlayerTrackingComponent");
+
+project.CreateModule("Touch");
+project.CreateService("Touch", "TrackingService");
+project.CreateComponent("Touch", "UGUITouchComponent");
+project.CreateComponent("Touch", "2DTouchComponent");
+project.CreateComponent("Touch", "3DTouchComponent");
+
+project.CreateModule("Pcc");
+project.CreateService("Pcc", "PccService");
+
+project.CreateModule("Control");
+project.CreateService("Control", "ControlService");
+project.CreateComponent("Control", "RuntimeControlUIComponent");
+project.CreateComponent("Control", "EditorControlUIComponent");
+
+project.CreateModule("Communication");
+project.CreateService("Communication", "FileIOService");
+project.CreateService("Communication", "JsonRPCService");
+project.CreateService("Communication", "WebSocketsService");
+
+project.CreateModule("Configuration");
+project.CreateService("Configuration", "PodConfigurationService");
+project.CreateService("Configuration", "GameConfigurationService");
+project.CreateService("Configuration", "SceneConfigurationService");
+
+project.CreateModule("ModLoader");
+project.CreateService("ModLoader", "ModLoaderService");
+
+project.CreateModule("Messaging");
+project.CreateService("Messaging", "MessageBroker");
+project.CreateComponent("Messaging", "MessageTriggerComponent");
+
+project.CreateModule("Session");
+project.CreateService("Session", "SessionService");
+project.CreateService("Session", "PlayerQueueService");
+project.CreateService("Session", "PlayerIdentificationService");
+project.CreateComponent("Session", "PlayerIdentifierComponent");
+
+project.CreateModule("Persistence");
+project.CreateService("Persistence", "PersistenceService");
+project.CreateComponent("Persistence", "PersistenceComponent");
+
+project.CreateModule("Audio");
+project.CreateService("Audio", "MusicPlayerService");
+project.CreateService("Audio", "UnityAudioService");
+project.CreateService("Audio", "FmodAudioService");
+
+project.CreateModule("Video");
+project.CreateComponent("Video", "VideoPlayerComponent");
+
+project.CreateModule("Content");
+project.CreateService("Content", "ContentProviderService");
+project.CreateService("Content", "StringContentService");
+project.CreateService("Content", "AudioContentService");
+project.CreateService("Content", "VideoContentService");
+project.CreateService("Content", "ImageContentService");
+
+project.CreateModule("Localization");
+project.CreateService("Localization", "LocalizationService");
+project.CreateService("Localization", "StringDataService");
+project.CreateService("Localization", "AudioDataService");
+project.CreateService("Localization", "VideoDataService");
+project.CreateService("Localization", "ImageDataService");
+project.CreateComponent("Localization", "LocalizedTMPComponent");
+project.CreateComponent("Localization", "LocalizedAudioSourceComponent");
+project.CreateComponent("Localization", "LocalizedSpriteRendererComponent");
+project.CreateComponent("Localization", "LocalizedUGUIImageComponent");
+project.CreateComponent("Localization", "LocalizedVideoComponent");
+
+
+project.CreateModule("DataCapture");
+project.CreateService("DataCapture", "DataCaptureService");
+project.CreateService("DataCapture", "ScoreDataCaptureService");
+project.CreateService("DataCapture", "TrackingDataCaptureService");
+project.CreateService("DataCapture", "TouchDataCaptureService");
+
+
+project.CreateModule("Logging");
+project.CreateService("Logging", "LoggingService");
+
+project.CreateModule("Notification");
+project.CreateService("Notification", "NotificationService");
+project.CreateComponent("Notification", "NotificationBarComponent");
+project.CreateComponent("Notification", "NotificationPopUpComponent");
+
+
+project.CreateModule("UI");
+project.CreateService("UI", "UGUIManagerService");
+project.CreateComponent("UI", "LayeredCanvasComponent");
+
+
+project.CreateModule("Build");
+project.CreateService("Build", "UnityPlayerBuildService");
+project.CreateService("Build", "UMod2BuildService");
