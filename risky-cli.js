d4136029ba885b9f214c149c56b3f5431364046c
Matt Taylor <matt.grs@googlemail.com>
2022-11-29 15:41:56 +0000

    setup for idlecom

--- risky-cli.js
+++ risky-cli.js
@@ -20,59 +20,18 @@ project.CreateModule("EditorTools");
 
 let projectConfig=
 {
-    name: "NFTGames",
+    name: "RiskyBusiness.IdleCommander",
     projectPaths: {
         Content: "{projectRoot}/Assets/Content",
         Scenes:  "{projectRoot}/Assets/Scenes",
-        Source:  "{projectRoot}/Assets/NFTGames",
-        Modules: "{projectRoot}/Assets/NFTGames/Common"
+        Source:  "{projectRoot}/Assets/Source/",
+        Modules: "{projectRoot}/Assets/Source/Modules"
     }
 }
 
 let project = new UnityProject(projectConfig);
 
-project.CreateModule("Services");
-project.CreateService("Services", "SessionService");
-project.CreateService("Services", "ScoreService");
-
-project.CreateModule("Messaging");
-
-project.CreateModule("UI");
-project.CreateComponent("UI", "ScoreUI");
-project.CreateComponent("UI", "AvatarSelectUI");
-project.CreateComponent("UI", "GameStartScreenUI");
-project.CreateComponent("UI", "GameOverScreenUI");
-
-project.CreateModule("Components");
-project.CreateComponent("Components", "Coin");
-project.CreateComponent("Components", "Player");
-project.CreateComponent("Components", "Level");
-project.CreateComponent("Components", "Obstacle");
-project.CreateComponent("Components", "EventZone");
-
-project.CreateModule("Input");
-project.CreateService("Input", "MouseInputService");
-project.CreateService("Input", "TouchInputService");
-project.CreateService("Input", "KeyboardInputService");
-
-project.CreateModule("NFTAPI");
-project.CreateModule("Ads");
+project.CreateModule("Data");
+project.CreateService("Data", "AssetData");
 
 
-projectConfig=
-    {
-        name: "NFTGames",
-        projectPaths: {
-            Content: "{projectRoot}/Assets/Content",
-            Scenes:  "{projectRoot}/Assets/Scenes",
-            Source:  "{projectRoot}/Assets/NFTGames/MiniGames",
-            Modules: "{projectRoot}/Assets/NFTGames/MiniGames"
-        }
-    }
-
-project = new UnityProject(projectConfig);
-
-project.CreateModule("EndlessRunner");
-project.CreateModule("DoodleJump");
-project.CreateModule("MiniMatch");
-project.CreateModule("FlappyBird");
